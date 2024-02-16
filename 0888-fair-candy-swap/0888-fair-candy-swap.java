79dd39834231ff1ef189a4e8a2552c4b42b76984
Abhik Khatuya <83873950+ABHIK-KHATUYA@users.noreply.github.com>
2024-02-16 15:45:39 +0530

    Time: 383 ms (8.46%), Space: 45.8 MB (59.81%) - LeetHub

---
+++ 0888-fair-candy-swap/0888-fair-candy-swap.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+        int ans[] = new int[2];
+        int a=0,b=0;
+        for(int i:aliceSizes){
+            a=a+i;
+        }
+        for(int i:bobSizes){
+            b=b+i;
+        }
+        int m = (a-b)/2;
+        for(int i=0;i<aliceSizes.length;i++){
+            for(int j=0;j<bobSizes.length;j++){
+                if(m > 0 && aliceSizes[i]-bobSizes[j] == m){
+                    ans[0]=aliceSizes[i];
+                    ans[1]=bobSizes[j];
+                   return ans;
+                } 
+                else if(m<0 && bobSizes[j]-aliceSizes[i] == Math.abs(m)){
+                    ans[0]=aliceSizes[i];
+                    ans[1]=bobSizes[j];
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+}
