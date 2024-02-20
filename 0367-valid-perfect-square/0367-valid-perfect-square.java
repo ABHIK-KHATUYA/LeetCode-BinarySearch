ebb59913ccf29cd81a968a6bff2efcc5094935ba
Abhik Khatuya <83873950+ABHIK-KHATUYA@users.noreply.github.com>
2024-02-20 13:03:22 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (96.01%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        double ans = Math.sqrt(num);
         
-        if(Math.sqrt(num)-(int)ans ==0){
+        if(Math.sqrt(num)-(int)Math.sqrt(num) ==0){
             return true;
         }
         else{
